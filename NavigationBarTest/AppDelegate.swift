31abb3ccb109bd641ab9e9ce4dc4afe6149a9e2b
Zeynal Zeynalov <zeynal.zeynalov@kapitalbank.az>
2023-04-19 15:36:11 +0400

    Initial commit

--- NavigationBarTest/AppDelegate.swift
+++ NavigationBarTest/AppDelegate.swift
@@ -9,12 +9,21 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        setupNavBarAppearance()
         return true
+    }
+
+    private func setupNavBarAppearance() {
+        let navBarAppearance = UINavigationBar.appearance()
+        navBarAppearance.tintColor = .blue
+        let titleTextAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 16),
+            .foregroundColor: UIColor.lightGray
+        ]
+        navBarAppearance.setBackgroundImage(UIImage(), for: .default)
+        navBarAppearance.shadowImage = UIImage()
+        navBarAppearance.titleTextAttributes = titleTextAttributes
     }
 
     // MARK: UISceneSession Lifecycle
@@ -30,7 +39,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
--- NavigationBarTest/ViewController.swift
+++ NavigationBarTest/ViewController.swift
@@ -7,13 +7,109 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var onViewWillAppear: (UIViewController) -> Void = { _ in }
+    var onViewWillDisappear: (UIViewController) -> Void = { _ in }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        onViewWillAppear(self)
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        onViewWillDisappear(self)
+    }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let text: String
+        switch indexPath.row {
+        case 0:
+            text = "Opaque nav bar"
+        case 1:
+            text = "white nav bar title"
+        default:
+            text = "invalid"
+        }
+        cell.textLabel?.text = text
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            navigationController?.pushViewController(.secondary(), animated: true)
+        case 1:
+            navigationController?.pushViewController(.ternary(), animated: true)
+        default:
+            break
+        }
+    }
 }
 
+extension UIViewController {
+    static func initial() -> UIViewController {
+        let controller = ViewController()
+        controller.view.backgroundColor = .white
+        controller.title = "Initial"
+        return controller
+    }
+
+    static func secondary() -> UIViewController {
+        let controller = ViewController()
+        controller.view.backgroundColor = .white
+        controller.title = "Opaque nav bar"
+        controller.onViewWillAppear = {
+            $0.setOpaqueNavBar()
+        }
+        controller.onViewWillDisappear = {
+            $0.resetNavBarOpaqueness()
+        }
+        return controller
+    }
+
+    static func ternary() -> UIViewController {
+        let controller = ViewController()
+        controller.view.backgroundColor = .lightGray
+        controller.onViewWillAppear = {
+            $0.setNavBarColorsToWhite()
+        }
+        controller.onViewWillDisappear = {
+            $0.resetNavBarColors()
+        }
+        controller.title = "white nav bar title"
+        return controller
+    }
+}
+
+extension UIViewController {
+    func updateNavBarAppearance(tintColor: UIColor, titleColor: UIColor) {
+        navigationController?.navigationBar.tintColor = tintColor
+        navigationController?.navigationBar.titleTextAttributes?[.foregroundColor] = titleColor
+    }
+
+    func setNavBarColorsToWhite() {
+        updateNavBarAppearance(tintColor: .white, titleColor: .white)
+    }
+
+    func resetNavBarColors() {
+        updateNavBarAppearance(tintColor: .blue, titleColor: .lightGray)
+    }
+
+    func setOpaqueNavBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        navigationController?.navigationBar.standardAppearance = appearance
+    }
+
+    func resetNavBarOpaqueness() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        navigationController?.navigationBar.standardAppearance = appearance
+    }
+}
